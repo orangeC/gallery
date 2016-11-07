a6e2579cd51a36b9e35150196642b8b952d7e164
orangeC <18332563755@163.com>
2016-11-07 19:23:01 +0800

    figure style

--- src/components/ImgFigure.js
+++ src/components/ImgFigure.js
@@ -4,12 +4,12 @@ import {render} from 'react-dom';
 class ImgFigure extends React.Component {
   render () {
     return(
-      <figure>
+      <figure className="img-figure">
         <img src={ this.props.data.imageURL }
              alt={ this.props.data.title }
         />
         <figcaption>
-          <h2>{ this.props.data.title }</h2>
+          <h2 className="img-title">{ this.props.data.title }</h2>
         </figcaption>
       </figure>
     )
